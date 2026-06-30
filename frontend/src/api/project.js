27562a4fd42e81d9bb8a204221bc4466d6e0ac52
PentagonCoder <harshu33908@gmail.com>
2026-06-30 12:37:49 +0000

    remved

--- frontend/src/api/project.js
+++
@@ -1,5 +0,0 @@
-import api from './axios'
-
-export const getMyProjects = (workspaceId) => api.get(`/project/my-projects/${workspaceId}`)
-
-export const createProject = (workspaceId, data) => api.post(`/project/create/${workspaceId}`, data)
--- frontend/src/api/workspace.js
+++
@@ -1,7 +0,0 @@
-import api from './axios'
-
-export const getMyWorkspaces = () => api.get('/workspace/my-workspaces')
-
-export const createWorkspace = (data) => api.post('/workspace/create', data)
-
-export const getWorkspaceById = (workspaceId) => api.get(`/workspace/${workspaceId}`)
